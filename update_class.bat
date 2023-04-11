310d60c7514e53fafc6912618ef638eef9e00147
Jaesung Son <77036161+JS970@users.noreply.github.com>
2023-04-12 01:07:55 +0900

    auto-update batch code

---
+++ update_class.bat
@@ -0,0 +1,36 @@
+@echo off
+
+set SRC_DIR=C:\SynologyDrive\obsidian\roundoff
+set DST_DIR=C:\Users\son73\Documents\GitHub\JS970.github.io
+
+set PL_SRC=%SRC_DIR%\PL
+set OS_SRC=%SRC_DIR%\OS
+set SSD_SRC=%SRC_DIR%\SSD
+set ALGO_SRC=%SRC_DIR%\Algorithm
+
+set PL_DST=%DST_DIR%\content
+set OS_IMG_DST=%DST_DIR%\static\image\OS
+set PL_IMG_DST=%DST_DIR%\static\image\PL
+set SSD_IMG_DST=%DST_DIR%\static\image\SSD
+set ALGO_IMG_DST=%DST_DIR%\static\image\Algorithm
+
+REM Copy all .md files from PL, OS, SSD, and Algorithm directories to the content directory
+xcopy /s /y %PL_SRC%\*.md %PL_DST%
+xcopy /s /y %OS_SRC%\*.md %PL_DST%
+xcopy /s /y %SSD_SRC%\*.md %PL_DST%
+xcopy /s /y %ALGO_SRC%\*.md %PL_DST%
+
+REM Copy all .png files from OS directory to the OS image destination directory
+xcopy /s /y %OS_SRC%\image\*.png %OS_IMG_DST%
+
+REM Copy all .png files from PL directory to the PL image destination directory
+xcopy /s /y %PL_SRC%\image\*.png %PL_IMG_DST%
+
+REM Copy all .png files from SSD directory to the SSD image destination directory
+xcopy /s /y %SSD_SRC%\image\*.png %SSD_IMG_DST%
+
+REM Copy all .png files from Algorithm directory to the Algorithm image destination directory
+xcopy /s /y %ALGO_SRC%\image\*.png %ALGO_IMG_DST%
+
+echo All files have been copied successfully!
+pause
