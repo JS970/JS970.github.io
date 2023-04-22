ded2da4e59d3caecaf28bd0e8c1a7f07abf679d2
손재성 <sonjaeseong@sonjaeseong-ui-MacBookAir.local>
2023-04-22 00:29:30 -0700

    mac env setting with .sh automation

---
+++ update_class.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/PL to ~/workspace/JS970.github.io/content
+cp -r ~/SynologyDrive/obsidian/roundoff/PL/* ~/workspace/JS970.github.io/content
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/OS to ~/workspace/JS970.github.io/content
+cp -r ~/SynologyDrive/obsidian/roundoff/OS/* ~/workspace/JS970.github.io/content
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/SSD to ~/workspace/JS970.github.io/content
+cp -r ~/SynologyDrive/obsidian/roundoff/SSD/* ~/workspace/JS970.github.io/content
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/Algorithm to ~/workspace/JS970.github.io/content
+cp -r ~/SynologyDrive/obsidian/roundoff/Algorithm/* ~/workspace/JS970.github.io/content
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/image/PL to ~/workspace/JS970.github.io/static/image/PL
+cp -r ~/SynologyDrive/obsidian/roundoff/image/PL/* ~/workspace/JS970.github.io/static/image/PL
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/image/OS to ~/workspace/JS970.github.io/static/image/OS
+cp -r ~/SynologyDrive/obsidian/roundoff/image/OS/* ~/workspace/JS970.github.io/static/image/OS
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/image/SSD to ~/workspace/JS970.github.io/static/image/SSD
+cp -r ~/SynologyDrive/obsidian/roundoff/image/SSD/* ~/workspace/JS970.github.io/static/image/SSD
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/image/Algorithm to ~/workspace/JS970.github.io/static/image/Algorithms
+cp -r ~/SynologyDrive/obsidian/roundoff/image/Algorithm/* ~/workspace/JS970.github.io/static/image/Algorithm
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/Enviromnent_Settings to ~/workspace/JS970.github.io/content
+cp -r ~/SynologyDrive/obsidian/roundoff/image/Enviromnent_Settings/* ~/workspace/JS970.github.io/content
+
+# Copy files from ~/SynologyDrive/obsidian/roundoff/image/Enviromnent_Settings to ~/workspace/JS970.github.io/static/image/Environment_Settings
+cp -r ~/SynologyDrive/obsidian/roundoff/image/Enviromnent_Settings/* ~/workspace/JS970.github.io/static/image/Enviromnent_Settings
